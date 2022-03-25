cbbef5faa5b7d4547c7d4500b06a40e9c5062139
Tomas <tomas.polacek.sv@gmail.com>
2022-03-25 18:31:11 +0100

    Added:  - sleep after uncollapse click  - sport, league, site  - 5, 2 number matches

--- scrapper_nike.py
+++ scrapper_nike.py
@@ -33,6 +33,7 @@ for idx_sport, sport in enumerate(sports):
     link = bs_sport.find('a', href=True)['href']
     sleep(randint(5,12))
     click = driver.find_element(by=By.XPATH, value ='//*[@id="left-column"]/div/div/div/div/div[4]/div/div[' + str(idx_sport + 1) + ']/div[1]/a[1]').click()
+    sleep(randint(1,2))
 
     # Extract element of each league
     leagues_bar =driver.find_element(by=By.XPATH, value ='//*[@id="left-column"]/div/div/div/div/div[4]/div/div[' + str(idx_sport + 1) + ']/div[2]').get_attribute('innerHTML') 
@@ -58,22 +59,49 @@ for idx_sport, sport in enumerate(sports):
         for row in rows:
             res = {}
             row_bs = BeautifulSoup(str(row).replace("\n", ""), 'html.parser')
-            possible_num = row_bs.find_all("span", class_="bet-center")
-            if len(possible_num) < 6:
-                break
-            res["1"] = possible_num[0].getText().strip() if possible_num[0].getText().strip() != "" else 0
-            res["0"] = possible_num[1].getText().strip() if possible_num[1].getText().strip() != "" else 0
-            res["2"] = possible_num[2].getText().strip() if possible_num[2].getText().strip() != "" else 0
-            res["10"] = possible_num[3].getText().strip() if possible_num[3].getText().strip() != "" else 0
-            res["12"] = possible_num[4].getText().strip() if possible_num[4].getText().strip() != "" else 0
-            res["20"] = possible_num[5].getText().strip() if possible_num[5].getText().strip() != "" else 0
 
+            # Numbers
+            possible_num = row_bs.find_all("span", class_="bet-center")
+            if len(possible_num) == 6:
+                # 1, 0, 2, 10, 12, 20
+                res["1"] = possible_num[0].getText().strip() if possible_num[0].getText().strip() != "" else 0
+                res["0"] = possible_num[1].getText().strip() if possible_num[1].getText().strip() != "" else 0
+                res["2"] = possible_num[2].getText().strip() if possible_num[2].getText().strip() != "" else 0
+                res["10"] = possible_num[3].getText().strip() if possible_num[3].getText().strip() != "" else 0
+                res["12"] = possible_num[4].getText().strip() if possible_num[4].getText().strip() != "" else 0
+                res["20"] = possible_num[5].getText().strip() if possible_num[5].getText().strip() != "" else 0
+
+            elif len(possible_num) == 5:
+                # 1, 0, 2, 10, 20
+                res["1"] = possible_num[0].getText().strip() if possible_num[0].getText().strip() != "" else 0
+                res["0"] = possible_num[1].getText().strip() if possible_num[1].getText().strip() != "" else 0
+                res["2"] = possible_num[2].getText().strip() if possible_num[2].getText().strip() != "" else 0
+                res["10"] = possible_num[3].getText().strip() if possible_num[3].getText().strip() != "" else 0
+                res["20"] = possible_num[4].getText().strip() if possible_num[4].getText().strip() != "" else 0
+
+            elif len(possible_num) == 2:
+                # 1, 2
+                res["1"] = possible_num[0].getText().strip() if possible_num[0].getText().strip() != "" else 0
+                res["2"] = possible_num[1].getText().strip() if possible_num[1].getText().strip() != "" else 0
+
+            else:
+                break
+
+            # Oponents
             oponents = row_bs.find("button", class_="bets-opponents ellipsis flex items-center").find_all("div")
             res["name1"] = oponents[0].getText().strip()
             res["name2"] = oponents[2].getText().strip()
 
+            # Time & date
             time_in_row =str(re.findall("\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}", str(row_bs))[0]) 
             res["date"] = datetime.strptime(time_in_row, '%d.%m.%Y %H:%M')
+
+            # Sport category and league (/tipovanie/futbal/anglicko/anglicko-i-liga)
+            parsed_link = link_league.split('/')
+            res["sport"] = parsed_link[2]
+            res["league"] = parsed_link[-1]
+
+            res["site"] = web_url
 
             results.append(res)
             print(res)
