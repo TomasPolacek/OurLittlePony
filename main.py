06da27a0bd8d2e9f91f38948439d9a9e7731d1ce
Tomas <tomas.polacek.sv@gmail.com>
2022-04-04 18:05:34 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import scrappers.scrapper_doxxbet as scr_doxx
 import scrappers.scrapper_nike as scr_nike
 import sys
 import datetime;
@@ -6,6 +7,20 @@ if __name__ == "__main__":
     
     # ct stores current time
     sys.stdout = open('log.txt', 'w', encoding="utf-8")
+
+    # Doxxbet scrapper
+    try: 
+        ct = datetime.datetime.now()
+        print("-----------------------------")
+        print("Start Nike script execution: ",ct)
+        scr_doxx.scrape(0)
+    except Exception as e:
+        print("ERROR: " + str(e))
+        print("End Nike script execution on error: ", datetime.datetime.now())
+    else:
+        print("End Nike script execution on success: ", datetime.datetime.now())    
+    print("Script execution: ", datetime.datetime.now() - ct)
+
     
     # Nike scrapper
     try: 
@@ -19,6 +34,7 @@ if __name__ == "__main__":
     else:
         print("End Nike script execution on success: ", datetime.datetime.now())    
     print("Script execution: ", datetime.datetime.now() - ct)
+
 
 
     sys.stdout.close()
